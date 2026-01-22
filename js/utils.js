89e116a266bbb0ccb9c218185316041d7663c6b4
Odudu02 <bisoalberti@gmail.com>
2026-01-22 01:03:58 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,3 @@
-// Algoritmo PRNG (Pseudo-Random Number Generator) determinístico
 export function createSeededRandom(seed) {
     let seedNum = 0;
     for (let i = 0; i < seed.length; i++) {
@@ -10,12 +9,4 @@ export function createSeededRandom(seed) {
         t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
         return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
     }
-}
-
-// Função auxiliar para gerar IDs únicos (UUID)
-export function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
 }
